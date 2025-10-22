1f9c382d259b951930761058ebed6eb9f95c720b
Guillaume Kuc <guillaume@guillaumekuc.com>
2025-10-22 10:26:47 +0200

    [update] keep it contained w/i execute()

--- src/actions/Shuffle.js
+++ src/actions/Shuffle.js
@@ -4,21 +4,21 @@ export default class Shuffle {
 
   static execute(store) {
     debugLog('Shuffling', store.filtered.length, 'filtered results');
-    const shuffled = this.shuffleArray(store.filtered);
+    const shuffled = shuffleArray(store.filtered);
     store.shuffled = shuffled;
+
+    function shuffleArray(arr) {
+      const a = [...arr];
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    }
   }
 
   static reset(store) {
     debugLog('Resetting shuffle - returning to normal order');
     store.shuffled = null;
-  }
-
-  static shuffleArray(arr) {
-    const a = [...arr];
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
-    }
-    return a;
   }
 }
